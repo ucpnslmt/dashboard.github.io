e96633417a965239f3179eebd676513bb2f4a7a1
ucpnslmt <ucpn.slmt@gmail.com>
2022-05-21 13:37:25 +0700

    Portfolio Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding:'16px'
+    },
+    extend: {
+      colors:{
+        primary: '#14B8A6',
+        dark: '#0F172A',
+        secondary: '#64748b',
+      },
+      screen:{
+        '2xl':'1320px',
+      },
+    },
+  },
+  plugins: [],
+}
